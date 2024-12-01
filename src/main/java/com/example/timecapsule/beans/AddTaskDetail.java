fc3017cca2f2f42b349513800e2ddd8753923c98
Johnathan Hawthorne <johnathan.hawthorne86@gmail.com>
2024-12-01 12:16:16 -0600

    added component stereotype to data classes

--- src/main/java/com/example/timecapsule/beans/AddTaskDetail.java
+++ src/main/java/com/example/timecapsule/beans/AddTaskDetail.java
@@ -1,9 +1,10 @@
 package com.example.timecapsule.beans;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+@Component
 public class AddTaskDetail {
     public String name;
     public int taskTypeId;
--- src/main/java/com/example/timecapsule/beans/UpdateTaskDetail.java
+++ src/main/java/com/example/timecapsule/beans/UpdateTaskDetail.java
@@ -1,7 +1,10 @@
 package com.example.timecapsule.beans;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 
+@Component
 public class UpdateTaskDetail {
     public long taskId;
     public long taskTypeId;
