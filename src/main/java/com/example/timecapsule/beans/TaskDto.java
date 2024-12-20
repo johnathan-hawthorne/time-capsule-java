9de5c1c39c0127e80a28d32e4a430bf022f1bf4f
Johnathan Hawthorne <hawthorne.johnathan@gmail.com>
2024-12-19 19:04:13 -0600

    refactored elapsed time formatting

--- src/main/java/com/example/timecapsule/beans/TaskDto.java
+++ src/main/java/com/example/timecapsule/beans/TaskDto.java
@@ -1,5 +1,6 @@
 package com.example.timecapsule.beans;
 
+import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.temporal.ChronoUnit;
@@ -20,20 +21,24 @@ public class TaskDto {
         DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
         this.startDateTime = task.startDateTime.format(formatter);
         this.endDateTime = task.endDateTime.format(formatter);
-        // TODO: time formatting needs work
-        String hours = task.elapsedTime.toHoursPart() < 10 ? "0" + task.elapsedTime.toHoursPart() : "" + task.elapsedTime.toHoursPart();
+        this.elapsedTime = FormatElapsedTime(task.elapsedTime);
+    }
+
+    private String FormatElapsedTime(Duration elapsedTime) {
+        String hours = elapsedTime.toHoursPart() < 10 ? "0" + elapsedTime.toHoursPart() : "" + elapsedTime.toHoursPart();
         String mins;
-        if (task.elapsedTime.toMinutesPart() >= 60) {
+        if (elapsedTime.toMinutesPart() >= 60) {
             mins = "00";
         } else {
-            mins = task.elapsedTime.toMinutesPart() < 10 ? "0" + task.elapsedTime.toMinutesPart() : "" + task.elapsedTime.toMinutesPart();
+            mins = elapsedTime.toMinutesPart() < 10 ? "0" + elapsedTime.toMinutesPart() : "" + elapsedTime.toMinutesPart();
         }
         String seconds;
-        if (task.elapsedTime.toSecondsPart() >= 60) {
+        if (elapsedTime.toSecondsPart() >= 60) {
             seconds = "00";
         } else {
-            seconds = task.elapsedTime.toSecondsPart() < 10 ? "0" + task.elapsedTime.toSecondsPart() : "" + task.elapsedTime.toSecondsPart();
+            seconds = elapsedTime.toSecondsPart() < 10 ? "0" + elapsedTime.toSecondsPart() : "" + elapsedTime.toSecondsPart();
         }
-        this.elapsedTime = String.format("%s:%s:%s", hours, mins, seconds);
+
+        return String.format("%s:%s:%s", hours, mins, seconds);
     }
 }
