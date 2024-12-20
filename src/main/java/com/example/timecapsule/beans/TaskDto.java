26b40f7ebfc100b7807858ef33463137c2955879
Johnathan Hawthorne <hawthorne.johnathan@gmail.com>
2024-12-19 18:45:42 -0600

    fixed elapsed time (for sure)

--- src/main/java/com/example/timecapsule/beans/TaskDto.java
+++ src/main/java/com/example/timecapsule/beans/TaskDto.java
@@ -2,6 +2,8 @@ package com.example.timecapsule.beans;
 
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 public class TaskDto {
     public long id;
@@ -19,18 +21,18 @@ public class TaskDto {
         this.startDateTime = task.startDateTime.format(formatter);
         this.endDateTime = task.endDateTime.format(formatter);
         // TODO: time formatting needs work
-        String hours = task.elapsedTime.toHours() < 10 ? "0" + task.elapsedTime.toHours() : "" + task.elapsedTime.toHours();
+        String hours = task.elapsedTime.toHoursPart() < 10 ? "0" + task.elapsedTime.toHoursPart() : "" + task.elapsedTime.toHoursPart();
         String mins;
-        if (task.elapsedTime.toMinutes() >= 60) {
+        if (task.elapsedTime.toMinutesPart() >= 60) {
             mins = "00";
         } else {
-            mins = task.elapsedTime.toMinutes() < 10 ? "0" + task.elapsedTime.toMinutes() : "" + task.elapsedTime.toMinutes();
+            mins = task.elapsedTime.toMinutesPart() < 10 ? "0" + task.elapsedTime.toMinutesPart() : "" + task.elapsedTime.toMinutesPart();
         }
         String seconds;
-        if (task.elapsedTime.toSeconds() >= 60) {
+        if (task.elapsedTime.toSecondsPart() >= 60) {
             seconds = "00";
         } else {
-            seconds = task.elapsedTime.toSeconds() < 10 ? "0" + task.elapsedTime.toSeconds() : "" + task.elapsedTime.toSeconds();
+            seconds = task.elapsedTime.toSecondsPart() < 10 ? "0" + task.elapsedTime.toSecondsPart() : "" + task.elapsedTime.toSecondsPart();
         }
         this.elapsedTime = String.format("%s:%s:%s", hours, mins, seconds);
     }
